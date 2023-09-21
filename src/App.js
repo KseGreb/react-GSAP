96a09d268461005c2604e1196f3d28a63acbed5b
“KseGreb” <ksegreb@gmail.com>
2023-09-21 14:09:09 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import gsap from 'gsap';
 import './App.css';
+import { useLayoutEffect, useRef } from 'react';
+import GreenSock from './GreenSock';
+
+
+
+
+
+
+const Box = ({ children, className, anim }) => {
+  return <div className={"box " + className } data-animate={ anim }>{children}</div>;
+};
 
 function App() {
+
+  const app = useRef();
+  
+  useLayoutEffect(() => {
+    
+    const ctx = gsap.context(() => {
+      // Target the two specific elements we have asigned the animate class
+      gsap.to("[data-animate='rotate']", {
+        rotation: 360,
+        repeat: -1,
+        repeatDelay: 1,
+        yoyo: true
+      });
+      
+      gsap.to("[data-animate='move']", {
+        x: 100,
+        repeat: -1,
+        repeatDelay: 1,
+        yoyo: true
+      });
+      
+      gsap.set(".dont-animate", {
+        backgroundColor: 'red'
+      });
+      
+    }, app);// <- Scope!
+    
+    return () => ctx.revert();
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app" ref={app}>
+      <Box anim="rotate">Box</Box>
+      <Box className="dont-animate">Don't Animate Me</Box>
+      <Box anim="move">Box</Box>
+
+<GreenSock/>
+
     </div>
   );
 }
